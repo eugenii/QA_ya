143f03508698e7e4640fd6cb5394ffe0c77f0ab7
eugenii <eugenii.cher@gmail.com>
2025-02-18 21:37:34 +0300

    return returns

--- qa_python_2/task_03.py
+++ qa_python_2/task_03.py
@@ -1,15 +1,16 @@
 # Задание 3. Классы для спортивных состязаний.
 points = 0
 
+
 class PointsForPlace:
     """Points for place."""
 
     @staticmethod
     def get_points_for_place(place):
         if place > 100:
-            print('Баллы начисляются только первым 100 участникам')
+            return 'Баллы начисляются только первым 100 участникам'
         if place < 1:
-            print('Спортсмен не может занять нулевое или отрицательное место')
+            return 'Спортсмен не может занять нулевое или отрицательное место'
         return 101 - place
 
 
@@ -19,8 +20,7 @@ class PointsForMeters:
     @staticmethod
     def get_points_for_meters(meters):
         if meters < 0:
-            print('Количество метров не может быть отрицательным')
-            return
+            return 'Количество метров не может быть отрицательным'
         return meters * 0.5
 
 
@@ -42,6 +42,6 @@ points_for_meters = PointsForMeters()
 print(points_for_meters.get_points_for_meters(10))
 
 total_points = TotalPoints()
-print(total_points.get_points_for_place(10))
+print(total_points.get_points_for_place(105))
 print(total_points.get_points_for_meters(10))
 print(total_points.get_total_points(100, 10))
